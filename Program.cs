ce8e363c8b93b9f5da0497c3c221b9fb8957abc3
Vladislav Churilov <Nawaharjan@gmail.com>
2024-07-19 08:23:15 +1000

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array =
+            {
+                { 1,3,4 },
+                { 2,4,8 },
+                { 3,6,12 }
+            };
+            Console.WriteLine(array[1,1]);
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for(int j = 0; j<array.GetLength(1); j++)
+                {
+                    Console.Write(array[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
